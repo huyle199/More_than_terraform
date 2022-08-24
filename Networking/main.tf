028096b8653a047a749da3a4ba58c9b415d410e0
Huy Le <86170240+huyle199@users.noreply.github.com>
2022-08-24 11:03:47 -0500

    Update main.tf

--- Networking/main.tf
+++ Networking/main.tf
@@ -1,1 +1,21 @@
 #Networking main
+
+
+resource "aws_security_group" "huy_sg"{
+  name = "public_sg"
+  description = "Security group for public access"
+  vpc_id = aws_vpc.huy_vpc.id
+  ingress{
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = [var.access_ip]
+  }
+  egress{
+    from_port = 0
+    to_port = 0
+    #protocol -1 is basically all protocols
+    protocol = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
