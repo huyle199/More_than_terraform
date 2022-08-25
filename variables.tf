62ad156cae9afdf7febd236114347fa57ee65a64
Huy Le <86170240+huyle199@users.noreply.github.com>
2022-08-25 14:23:54 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,21 @@
 variable "aws_region" {
   default = "us-east-1"
 }
+
+variable "access_ip"{
+  type = string
+}
+
+variable "dbname"{
+  type = "string"
+}
+
+variable "dbuser"{
+  type = "string"
+  sensitive = true
+}
+
+variable "dbpassword"{
+  type = "string"
+  sensitive = true
+}
