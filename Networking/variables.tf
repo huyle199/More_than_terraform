1bf462b1d9eecca2da4363ca787c5e9aa473d112
Huy Le <86170240+huyle199@users.noreply.github.com>
2022-08-24 19:28:03 -0500

    Update variables.tf

--- Networking/variables.tf
+++ Networking/variables.tf
@@ -1,3 +1,29 @@
+#--network variables
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "public_cidrs" {
+    type = list
+  
+}
+
+variable "private_cidrs" {
+    type = list
+  
+}
+variable "public_sn_count" {
+  type = number
+}
+
+variable "private_sn_count" {
+  type = number
+}
+
+variable "max_subnets" {
+  type = number
+}
 
 variable "access_ip" {
   type = string
