9f8b46c4bcee8d5a9d9396e05a1b5813f6995030
Huy Le <86170240+huyle199@users.noreply.github.com>
2022-08-25 12:01:38 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,3 +10,18 @@ module "networking"{
   private_cidrs = [for   i in range(2,255,2) : cidrsubnet(local.vpc_cidr,8,i)]
   db_subnet_group = true
   }
+    
+module "database"{
+  source = "./database"
+  db_storage = 10
+  db_engine_version = "5.7.22"
+  db_instance_class = "db.t2.micro"
+  dbname = "rancher"
+  dbuser = "bobby"
+  db_password = "t4b!3s2021"
+  db_identifier = "huy_db"
+  skip_db_snapshot = true
+  db_subnet_group_name = ""
+  vpc_security_group_ids = []
+}
+  
