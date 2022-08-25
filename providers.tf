53beeb58e8ab355153f236453f711560e1e0125c
Huy Le <86170240+huyle199@users.noreply.github.com>
2022-08-24 19:25:45 -0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,1 +1,13 @@
+terraform {
+  required_providers{
+    aws = {
+        source = "hashicorp/aws"
+    }
+  }
+}
 
+#configure the AWS provider
+provider "aws" {
+    region = var.aws_region
+  
+}
