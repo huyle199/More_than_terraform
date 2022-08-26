47fcaf80dcb40706d40e5bc7b8758a156da48222
Huy Le <86170240+huyle199@users.noreply.github.com>
2022-08-26 10:55:07 -0500

    Update main.tf

--- loadbalancing/main.tf
+++ loadbalancing/main.tf
@@ -5,3 +5,15 @@ resource "aws_lb" "huy_lb"{
   security_groups = [var.public_sg]
   idle_timeout = 400
 }
+
+resource "aws_lb_target_group" "huy_tg"{
+  name = "huy-lb-tg-$(substr(uuid(),0,3"
+  port = var.tg_port #80
+  protocol = var.tg_protocol #"HTTP"
+  vpc_id = var.vpc_id
+  health_check{
+    healthy_threshold = var.lb_healthy_threshold
+    unhealthy_threshold = var.lb_unhealthy_threshold
+    timeout = var.lb_timeout
+    interval =  var.lb_interval
+}
