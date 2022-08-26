f39482c04a43ceef7d49d7ae52487f87d193a2d0
Huy Le <86170240+huyle199@users.noreply.github.com>
2022-08-26 10:09:13 -0500

    Update outputs.tf

--- Networking/outputs.tf
+++ Networking/outputs.tf
@@ -8,3 +8,11 @@ output "db_subnet_group_name"{
 output "db_security_group"{
   value = [aws_security_group.huy_sg["rds"].id]
 }
+
+output "public_sg"{
+  value = aws_security_group.huy_sg["public"].id
+}
+
+output "public_subnets"{
+  value = aws_subnets.huy_public_subnet.*.id
+}
