92e96742309e0820b36df386756218ee78e727ae
Huy Le <86170240+huyle199@users.noreply.github.com>
2022-08-26 11:03:17 -0500

    Update variables.tf

--- loadbalancing/variables.tf
+++ loadbalancing/variables.tf
@@ -1,3 +1,10 @@
 #loadbalancing variables
-variables "public_sg"{}
-variables "public_subnets"{}
+variable "public_sg"{}
+variable "public_subnets"{}
+variable "tg_port"{}
+variable "tg_protocol"{}
+variable "vpc_id"{}
+variable "lb_healthy_threshold"{}
+variable "lb_unhealthy_threshold:"{}
+variable "lb_timeout"{}
+variable "lb_interval"{}
